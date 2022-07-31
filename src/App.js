6e21cc8ae608a3a87436ea8948b3751f7539e0fb
ItaloMirandaSantiago <italo.miranda8635@gmail.com>
2022-07-31 18:31:31 -0300

    fundo dividido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './style.scss/Main.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="menu">
+            <img />
+            <div>
+              <a href="/">Home</a>
+              <a href="/">Sobre</a>
+              <a href="/">Projetos</a>
+            </div>
+        </div>
+        <div className="title">
+          <h2>Olá! Meu nome é Italo Miranda</h2>
+          <p>Programador Front-end</p>
+        </div>
+        <img width={"300px"} src="https://cdn-icons.flaticon.com/png/512/6061/premium/6061291.png?token=exp=1659237383~hmac=23ee84addf070e5c2939716474237d0f" />
     </div>
   );
 }
