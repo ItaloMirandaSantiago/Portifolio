e25ff222963a27c13269810ce47c167e3f027cb1
ItaloMirandaSantiago <italo.miranda8635@gmail.com>
2022-08-02 20:16:13 -0300

    base dos projetos feito - não responsivo

--- src/App.js
+++ src/App.js
@@ -16,8 +16,23 @@ function App() {
             <h2>Olá! Meu nome é Italo Miranda</h2>
             <p>Programador Front-end</p>
           </div>
+
           <div>
-          <img width={"300px"} src="https://cdn-icons.flaticon.com/png/512/6061/premium/6061291.png?token=exp=1659237383~hmac=23ee84addf070e5c2939716474237d0f" alt="imagem decorativa"/>
+            <img width={"300px"} src="https://cdn-icons.flaticon.com/png/512/6061/premium/6061291.png?token=exp=1659237383~hmac=23ee84addf070e5c2939716474237d0f" alt="imagem decorativa"/>
+          </div>
+        </div>
+
+        <div className="title">
+          <div>
+              <p>
+                O projeto foi feito com o intuito de treinar chamadas de API e mostrar na tela. 
+              </p>
+          </div>
+
+          <div>
+            <div className="containerProjeto">
+
+            </div>
           </div>
         </div>
     </div>
