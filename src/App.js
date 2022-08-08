6f0ec44d451455c23481b4375d4fa97f361ddfdb
ItaloMirandaSantiago <italo.miranda8635@gmail.com>
2022-08-08 18:42:14 -0300

    melhorando a responsividade e testando forma dos projetos

--- src/App.js
+++ src/App.js
@@ -18,23 +18,37 @@ function App() {
           </div>
 
           <div>
-            <img width={"300px"} src="https://cdn-icons.flaticon.com/png/512/6061/premium/6061291.png?token=exp=1659237383~hmac=23ee84addf070e5c2939716474237d0f" alt="imagem decorativa"/>
+            <img width={"100%"} src="https://cdn-icons.flaticon.com/png/512/6061/premium/6061291.png?token=exp=1659237383~hmac=23ee84addf070e5c2939716474237d0f" alt="imagem decorativa"/>
           </div>
         </div>
 
         <div className="title">
-          <div>
-              <p>
-                O projeto foi feito com o intuito de treinar chamadas de API e mostrar na tela. 
-              </p>
-          </div>
-
-          <div>
-            <div className="containerProjeto">
-
+            <div>
+                <p>
+                  O projeto foi feito com o intuito de treinar chamadas de API e mostrar na tela. 
+                </p>
             </div>
-          </div>
+
+            <div>
+                <iframe id="animes" className="containerProjeto" src="https://italomirandasantiago.github.io/site_de_animes/"/>
+            </div>
         </div>
+
+        <div className="title">
+
+            <div>
+                <iframe id="noticias" className="containerProjeto" src="https://italomirandasantiago.github.io/site_de_noticias/" />
+            </div>
+
+            <div>
+                <p>
+                  Site de noticias curtas! Saiba o que acontece pelo mundo sem perder tempo!
+                </p>
+            </div>
+        </div>
+
+
+
     </div>
   );
 }
